084c5557a85078643baf167241d093612f87220b
pranavdarji <51908377+pranavdarji@users.noreply.github.com>
2019-07-11 20:27:39 +0530

    Add files via upload

--- contact_process.php
+++ contact_process.php
@@ -1,19 +1,19 @@
 <?php
 
-    $to = "pdarji576s@gmail.com";
+    $to = "pdarji576@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
     $number = $_REQUEST['number'];
     $cmessage = $_REQUEST['message'];
 
-    $headers = "From:" $from;
-	$headers = "From: " . $from . "\r\n";
+    $headers = "From:" . $from;
+	$headers = "From: " . $to . "\r\n";
 	$headers .= "Reply-To: ". $from . "\r\n";
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Your Portfolio.";
+    $subject = "You have a message from your Portfolio.";
 
     $logo = 'img/logo.png';
     $link = '#';
