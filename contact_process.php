06154b0073724e02595d108dd1637f706bb39819
pranavdarji <51908377+pranavdarji@users.noreply.github.com>
2019-07-11 20:52:40 +0530

    Add files via upload

--- contact_process.php
+++ contact_process.php
@@ -8,7 +8,7 @@
     $cmessage = $_REQUEST['message'];
 
     $headers = "From:" . $from;
-	$headers = "From: " . $to . "\r\n";
+	$headers .= "To:" . $to . "\r\n";
 	$headers .= "Reply-To: ". $from . "\r\n";
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
